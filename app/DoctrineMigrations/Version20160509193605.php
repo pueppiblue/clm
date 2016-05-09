1e7adb04421d08e0493c7d9969e47aacbd3bda23
Nils Freigang <Nils.Freigang@gmx.de>
2016-05-09 20:21:49 +0200

    add new migration file to reflect changed property names

--- app/DoctrineMigrations/Version20160509193605.php
+++ app/DoctrineMigrations/Version20160509193605.php
@@ -8,7 +8,7 @@ use Doctrine\DBAL\Schema\Schema;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-class Version20160506165702 extends AbstractMigration
+class Version20160509193605 extends AbstractMigration
 {
     /**
      * @param Schema $schema
@@ -18,11 +18,11 @@ class Version20160506165702 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, tear INT DEFAULT NULL, acc INT DEFAULT NULL, weapon INT DEFAULT NULL, item INT DEFAULT NULL, urn INT DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE item (id INT AUTO_INCREMENT NOT NULL, looter_id INT DEFAULT NULL, name VARCHAR(255) NOT NULL, drop_date DATETIME NOT NULL, category VARCHAR(255) DEFAULT NULL, isCash TINYINT(1) DEFAULT NULL, INDEX IDX_1F1B251EB13C2BDA (looter_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE clm_account (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, tear INT DEFAULT NULL, acc INT DEFAULT NULL, weapon INT DEFAULT NULL, item INT DEFAULT NULL, urn INT DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
         $this->addSql('CREATE TABLE clm_characters (id INT AUTO_INCREMENT NOT NULL, user_id INT DEFAULT NULL, name VARCHAR(255) NOT NULL, clm_class VARCHAR(255) NOT NULL, preferred_set VARCHAR(255) DEFAULT NULL, INDEX IDX_3156DDECA76ED395 (user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
-        $this->addSql('ALTER TABLE item ADD CONSTRAINT FK_1F1B251EB13C2BDA FOREIGN KEY (looter_id) REFERENCES user (id)');
-        $this->addSql('ALTER TABLE clm_characters ADD CONSTRAINT FK_3156DDECA76ED395 FOREIGN KEY (user_id) REFERENCES user (id)');
+        $this->addSql('CREATE TABLE clm_item (id INT AUTO_INCREMENT NOT NULL, looter_id INT DEFAULT NULL, name VARCHAR(255) NOT NULL, drop_date DATETIME NOT NULL, category VARCHAR(255) DEFAULT NULL, isCash TINYINT(1) DEFAULT NULL, INDEX IDX_4726E03CB13C2BDA (looter_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE clm_characters ADD CONSTRAINT FK_3156DDECA76ED395 FOREIGN KEY (user_id) REFERENCES clm_account (id)');
+        $this->addSql('ALTER TABLE clm_item ADD CONSTRAINT FK_4726E03CB13C2BDA FOREIGN KEY (looter_id) REFERENCES clm_account (id)');
     }
 
     /**
@@ -33,10 +33,10 @@ class Version20160506165702 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE item DROP FOREIGN KEY FK_1F1B251EB13C2BDA');
         $this->addSql('ALTER TABLE clm_characters DROP FOREIGN KEY FK_3156DDECA76ED395');
-        $this->addSql('DROP TABLE user');
-        $this->addSql('DROP TABLE item');
+        $this->addSql('ALTER TABLE clm_item DROP FOREIGN KEY FK_4726E03CB13C2BDA');
+        $this->addSql('DROP TABLE clm_account');
         $this->addSql('DROP TABLE clm_characters');
+        $this->addSql('DROP TABLE clm_item');
     }
 }
