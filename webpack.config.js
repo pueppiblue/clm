310805bd9c66fe4c48f4a7a95d5ba8905c816f93
pueppiblue <Nils.Freigang@gmx.de>
2016-05-10 16:04:56 +0200

    add webpack config and dummy source assets #13

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+var path = require('path');
+var webpack = require('webpack');
+var source_dir = path.join(__dirname, 'app/Resources/AppBundle/assets');
+var dest_dir = path.join(__dirname, 'web');
+
+module.exports = {
+    entry: [
+        path.join(source_dir,"js/hello.js")
+        ],
+    output: {
+        path: dest_dir,
+        filename: "clm_main.js"
+    },
+    module: {
+        loaders: [
+            {test: /\.css$/, loader: "style!css"}
+        ]
+    }
+};
