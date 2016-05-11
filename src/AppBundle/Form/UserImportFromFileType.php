53977822587cfbe9ba1ceafaea3293afee56b98d
pueppiblue <Nils.Freigang@gmx.de>
2016-05-12 01:02:02 +0200

    add a form type class for  the import of users #9
      The form only provides a FileType field.

---
+++ src/AppBundle/Form/UserImportFromFileType.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace AppBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+
+
+class UserImportFromFileType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $formBuilder, array $options)
+    {
+        $formBuilder
+            ->add("XmlFile", FileType::class);    
+    }
+    
+
+}
