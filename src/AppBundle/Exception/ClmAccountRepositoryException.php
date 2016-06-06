89ccc0e4e68202e64d255e599098f1d4c0012cd0
pueppiblue <Nils.Freigang@gmx.de>
2016-06-06 09:39:10 +0200

    change exception typehint to Exception

--- src/AppBundle/Exception/ClmAccountRepositoryException.php
+++ src/AppBundle/Exception/ClmAccountRepositoryException.php
@@ -3,19 +3,19 @@
 namespace AppBundle\Exception;
 
 
-use Doctrine\DBAL\Exception\UniqueConstraintViolationException;
 use Symfony\Component\Config\Definition\Exception\Exception;
 
 class ClmAccountRepositoryException extends \Exception
 {
 
+
     /**
      * ClmAccountRepositoryException constructor.
      * @param string $message
      * @param int $code
-     * @param UniqueConstraintViolationException|\Exception|Exception $e
+     * @param Exception $e
      */
-    public function __construct($message = "", $code = 0, UniqueConstraintViolationException $e = null)
+    public function __construct($message = "", $code = 0, Exception $e = null)
     {
         parent::__construct($message, $code, $e);
     }
