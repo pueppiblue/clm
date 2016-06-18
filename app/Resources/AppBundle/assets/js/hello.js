94fd1a8068a1dd9303a00fe216976755a80dd104
pueppiblue <Nils.Freigang@gmx.de>
2016-06-18 22:32:27 +0200

    add sidenav javascript to custom js file

---
+++ app/Resources/AppBundle/assets/js/hello.js
@@ -0,0 +1,7 @@
+// materialize-css element functions
+$(document).ready(function(){
+    $(".button-collapse").sideNav({
+        menuWidth: 200 // Default is 240
+    });
+    $('.collapsible').collapsible();
+});
