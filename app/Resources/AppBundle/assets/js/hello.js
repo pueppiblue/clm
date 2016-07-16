7c66864e905ad6ca895d9a4bb5f685254f9ef7e9
pueppiblue <Nils.Freigang@gmx.de>
2016-07-16 20:18:37 +0200

    add JavaScript for drag n drop functionality #18

--- app/Resources/AppBundle/assets/js/hello.js
+++ app/Resources/AppBundle/assets/js/hello.js
@@ -5,3 +5,20 @@ $(document).ready(function(){
     });
     $('.collapsible').collapsible();
 });
+
+function dragStart(ev) {
+    ev.DataTransfer.effectAllowed='move';
+    ev.DataTransfer.setData("Text", ev.target.getAttribute('id'));
+    ev.DataTransfer.setDragImage(ev.target, 100, 100);
+    return true;
+}
+
+function dragEnter(ev) {
+    ev.preventDefault();
+    return true;
+}
+
+function dragOver(ev) {
+    ev.preventDefault();
+}
+
