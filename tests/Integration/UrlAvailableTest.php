15d48d1d01538538001c08f170bcef001462e276
pueppiblue <Nils.Freigang@gmx.de>
2016-07-22 22:58:44 +0200

    add UrlAvailableTest class #28
       The class tests if the paths for the routes in routing.yml
        are reachable.

---
+++ tests/Integration/UrlAvailableTest.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Tests\AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class UrlAvailable extends WebTestCase
+{
+    /**
+     * @dataProvider urlProvider
+     * @param $url
+     */
+    public function testPathIsAvailable($url)
+    {
+        $client = self::createClient();
+        $client->request('GET', $url);
+
+        static::assertTrue($client->getResponse()->isSuccessful(),
+            'URL: ' . $url . ' could not be reached successfully.');
+    }
+
+    /**
+     * @dataProvider wildURLProvider
+     * @param String $url
+     */
+    public function testWildCardsAreRedirected($url)
+    {
+        $client = self::createClient();
+        $client->request('GET', $url);
+
+        static::assertTrue($client->getResponse()->isRedirection(),
+            "url with wildcards was not redirected.");
+    }
+
+    /**
+     * @return array
+     */
+    public function wildURLProvider()
+    {
+        return [
+            ['/kashasjkhdaslhkd/asdasldkha'],
+            ['/lsjdoasdu']
+        ];
+    }
+
+    /**
+     * @return array
+     */
+    public function urlProvider()
+    {
+        return [
+            ['/user/list'],
+            ['/user/import'],
+            ['/user/show/1'],
+            ['/raid/show/1'],
+        ];
+    }
+}
