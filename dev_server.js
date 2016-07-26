4ea94e011194c2b61e053ccb686d4ed74a4d9d59
pueppiblue <Nils.Freigang@gmx.de>
2016-07-26 18:15:10 +0200

    move webpack_dev_server config to separate file #25
      webpack dev server can now be loaded via dev_server.js
      the inline mode for hmr isnt present in webpack.config but is rather
      appended with "unshift" in the dev_server config.
      Also removed some options from url-loader loader.

---
+++ dev_server.js
@@ -0,0 +1,30 @@
+var config = require("./webpack.config.js");
+var webpack = require('webpack');
+var port = 8090;
+
+var webpackDevServer=require('webpack-dev-server');
+
+config.entry.main.unshift(
+    "webpack-dev-server/client?http://localhost:" +
+    port + "/", "webpack/hot/dev-server"
+);
+
+var devServer = new webpackDevServer(webpack(config), {
+        hot: true,
+        publicPath: "http://localhost:" + port + "/assets/",
+        contentBase: "web/",
+        headers: {"X-Custom-Header": "yes"},
+        stats: {colors: true}
+    }
+);
+
+devServer.listen(
+    port,
+    'localhost',
+    function (err, result) {
+        if (err) {
+            console.log(err);
+        }
+        console.log('Listening at localhost:' + port);
+    }
+);
