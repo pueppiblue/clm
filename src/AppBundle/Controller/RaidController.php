5d4abc0a1e15f2d4fd12bd63b254ca5018059355
pueppiblue <Nils.Freigang@gmx.de>
2016-08-07 13:10:37 +0200

    add fields and doc blocks to RaidController ä#15

--- src/AppBundle/Controller/RaidController.php
+++ src/AppBundle/Controller/RaidController.php
@@ -6,11 +6,35 @@ namespace AppBundle\Controller;
 use AppBundle\Service\UserLootManager;
 use Symfony\Bundle\FrameworkBundle\Templating\EngineInterface;
 use Symfony\Component\Form\FormFactory;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 class RaidController
 {
+    /**
+     * @var UserLootManager
+     */
+    private $userLootManager;
+    /**
+     * @var EngineInterface
+     */
+    private $templating;
+    /**
+     * @var UrlGeneratorInterface
+     */
+    private $router;
+    /**
+     * @var FormFactory
+     */
+    private $formFactory;
 
+    /**
+     * RaidController constructor.
+     * @param EngineInterface $templating
+     * @param UrlGeneratorInterface $router
+     * @param FormFactory $formFactory
+     * @param UserLootManager $userLootManager
+     */
     public function __construct(
         EngineInterface $templating,
         UrlGeneratorInterface $router,
@@ -23,11 +47,26 @@ class RaidController
         $this->formFactory = $formFactory;
     }
 
-    public function showAction($id=1)
+    /**
+     * @param int $id
+     * @return Response
+     */
+    public function createRosterAction()
+    {
+        return $this->templating->renderResponse(
+            ':raid:createRoster.html.twig'
+        );
+
+    }
+
+    /**
+     * @param int $id
+     * @return Response
+     */
+    public function showAction()
     {
         return $this->templating->renderResponse(
             ':raid:show.html.twig'
         );
-        
     }
 }
